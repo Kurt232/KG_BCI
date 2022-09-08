605735bfa95502cba41e7024ae7c56ea142dfde4
duwsl2 <duwenjie@stu.scu.edu.cn>
2022-09-08 21:26:29 +0800

    暂无数据集

---
+++ download_bert_chinese.sh
@@ -0,0 +1,4 @@
+mkdir bert-base-chinese
+wget -P bert-base-chinese https://huggingface.co/bert-base-chinese/resolve/main/config.json
+wget -P bert-base-chinese https://huggingface.co/bert-base-chinese/resolve/main/pytorch_model.bin
+wget -P bert-base-chinese https://huggingface.co/bert-base-chinese/resolve/main/vocab.txt
---
+++ nre/model/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .softmax_nn import SoftmaxNN
+
+__all__ = [
+    'SentenceRE',
+]
